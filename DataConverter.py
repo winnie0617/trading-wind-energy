f01d04c5fa74bf2cb0fb475b8495010604d8e6de
issacto <54349664+issacto@users.noreply.github.com>
2020-07-11 13:00:38 +0800

    update

--- DataConverter.py
+++ DataConverter.py
@@ -2,6 +2,9 @@ from csv import writer
 import csv
 import datetime
 
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#note that time zone is a problem now the time starts at 8:00
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 with open('/Users/issac/Documents/GitHub/trading-wind-energy/rawData.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -13,8 +16,6 @@ with open('DataWithNormalTime.csv','a+',  newline='') as write_obj:
         csv_writer = writer(write_obj)
         array = []
         array = row.split('=')
-        print(int(array[0][0:11]))
         newTime = datetime.datetime.fromtimestamp(int(array[0][0:11]))
         print(newTime)
-        break
         csv_writer.writerow([newTime.strftime('%Y-%m-%d %H:%M:%S'),array[1]])
