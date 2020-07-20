6d6b4bf4a15027c08bf63d4a387e4909c3163063
winniechow0617 <wc459@cornell.edu>
2020-07-21 00:16:33 +0800

    updated scheduler

--- Prediction.py
+++ Prediction.py
@@ -1,29 +1,23 @@
-# from keras.models import load_model
-# import pandas as pd
-# from datetime import datetime
-# import webbrowser
+'''
+from keras.models import load_model
+import pandas as pd
+from datetime import datetime
+import webbrowser
 import urllib.request
-import requests
 import datetime
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
+'''
+import requests
 import schedule
 import time
 from datetime import datetime
 from preprocess import get_average_speed, get_average_direction
 
+'''
 model = load_model('trading_model.h5')
 
-csv_list = ['angerville-1.csv', 'angerville-1-b.csv', 'angerville-2.csv', 'angerville-2-b.csv', 'arville.csv', 'arville-b.csv', 'boissy-la-riviere.csv', 'boissy-la-riviere-b.csv',
-            'guitrancourt.csv', 'guitrancourt-b.csv', 'lieusaint.csv', 'lieusaint-b.csv', 'lvs-pussay.csv', 'lvs-pussay-b.csv', 'parc-du-gatinais.csv', 'parc-du-gatinais-b.csv']
 
-
-def update_data(list):
-    for csv in list:
-        url = 'https://ai4impact.org/P003/historical/'+csv
-        r = requests.get(url, allow_redirects=True)
-        open('AppendixData/'+csv, 'wb').write(r.content)
-        print('Dataset ' + csv + ' updated')
 
 def scale_data(data):
     scaler = MinMaxScaler()
@@ -129,6 +123,17 @@ while True:
     print(value)
     webbrowser.open("http://3.1.52.222/submit/pred?pwd=7351140636&value="+value)
 
+'''
+csv_list = ['angerville-1.csv', 'angerville-1-b.csv', 'angerville-2.csv', 'angerville-2-b.csv', 'arville.csv', 'arville-b.csv', 'boissy-la-riviere.csv', 'boissy-la-riviere-b.csv',
+            'guitrancourt.csv', 'guitrancourt-b.csv', 'lieusaint.csv', 'lieusaint-b.csv', 'lvs-pussay.csv', 'lvs-pussay-b.csv', 'parc-du-gatinais.csv', 'parc-du-gatinais-b.csv']
+
+def update_data(list):
+    for csv in list:
+        url = 'https://ai4impact.org/P003/historical/'+csv
+        r = requests.get(url, allow_redirects=True)
+        open('AppendixData/'+csv, 'wb').write(r.content)
+        print('Dataset ' + csv + ' updated')
+
 
 def predict():
     print(datetime.now())
@@ -136,7 +141,7 @@ def predict():
     get_average_speed(csv_list)
     get_average_direction(csv_list)
 
-schedule.every(10).seconds.do(predict)
+schedule.every().hour.at(':15').do(predict)
 
 while True:
     schedule.run_pending()
