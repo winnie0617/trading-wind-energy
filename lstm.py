62e1e2425d1d4e2903cd1f56f77aff3b6b79b922
winniechow0617 <wc459@cornell.edu>
2020-07-30 13:45:30 +0800

    Clean up

--- lstm.py
+++ lstm.py
@@ -20,13 +20,14 @@ from keras.regularizers import l2
 
 
 # Configuration
-BATCH_SIZE = 32
+BATCH_SIZE = 320
 TIMESTEPS = 24
 EPOCH = 100
 PATIENCE = 10
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.01
 
 # Get energy production, wind speed and wind direction data
+
 
 def scale_data(data):
     scaler = MinMaxScaler()
@@ -44,7 +45,8 @@ speeds = df_speed['Average Speed (m/s)'].to_numpy().reshape(-1, 1)
 speeds_scaled = scale_data(speeds)
 
 df_direction = pd.read_csv('../trading-wind-energy/average-wind-direction.csv')
-directions = df_direction['Direction (deg N)'].to_numpy().reshape(-1, 1)
+directions = df_direction['Average Direction (deg N)'].to_numpy(
+).reshape(-1, 1)
 directions_scaled = scale_data(directions)
 
 
@@ -53,9 +55,6 @@ num_inputs = speeds_scaled.shape[0]
 energys_shifted = df_energy['Energy Production (kWh)'].shift(
     periods=-18)[TIMESTEPS-1:num_inputs-18].to_numpy().reshape(-1, 1)
 energys_shifted_scaled = scale_data(energys_shifted)
-# print("e shifted" + str(energys_shifted[:20, 0]))
-# print(energys_shifted.shape)
-# print("e shifted" + str(energys_shifted[30810:]))
 
 
 # Get max energy production in the window
@@ -73,7 +72,7 @@ max_energy_scaled = scale_data(max_energy)
 min_energy = []
 for i in range(num_inputs):
     if i < TIMESTEPS:
-        min = df_energy['Energy Production (kWh)'][0:i+1].min() 
+        min = df_energy['Energy Production (kWh)'][0:i+1].min()
     else:
         min = df_energy['Energy Production (kWh)'][i-TIMESTEPS:i+1].min()
     min_energy.append(min)
@@ -88,9 +87,6 @@ for i in range(1, num_inputs):
     difference.append(
         df_energy['Energy Production (kWh)'][i] - df_energy['Energy Production (kWh)'][i-1])
 difference = np.array(difference).reshape(-1, 1)
-# scaler = MinMaxScaler((-1,1))
-# scaler.fit(difference)
-# difference_scaled = scaler.transform(difference)
 difference_scaled = scale_data(difference)
 print(difference[:20])
 
@@ -99,7 +95,7 @@ print(difference[:20])
 mean_energy = []
 for i in range(num_inputs):
     if i < TIMESTEPS:
-        mean = df_energy['Energy Production (kWh)'][0:i+1].mean() 
+        mean = df_energy['Energy Production (kWh)'][0:i+1].mean()
     else:
         mean = df_energy['Energy Production (kWh)'][i-TIMESTEPS:i+1].mean()
     mean_energy.append(mean)
@@ -110,7 +106,7 @@ mean_energy_scaled = scale_data(mean_energy)
 sd_energy = []
 for i in range(num_inputs):
     if i == 0:
-        sd =0
+        sd = 0
     elif i < TIMESTEPS:
         sd = df_energy['Energy Production (kWh)'][0:i+1].std()
     else:
@@ -127,7 +123,7 @@ print(max_energy_scaled.shape)
 print(energys_scaled.shape)
 for i in range(num_inputs):
     x[i] = np.concatenate((energys_scaled[i], max_energy_scaled[i], min_energy_scaled[i], difference_scaled[i], mean_energy_scaled[i],
-                     speeds_scaled[i], directions_scaled[i]))
+                           speeds_scaled[i], directions_scaled[i]))
 print(x[:10])
 print("x" + str(x.shape))
 
@@ -137,40 +133,24 @@ x_transformed = []
 for i in range(TIMESTEPS, num_inputs):
     x_transformed.append(x[i-TIMESTEPS:i, :])
 x_transformed = np.array(x_transformed)
-# print(x_transformed[:10])
-# print("x_transformed" + str(x_transformed.shape))
-# print(x_transformed[x_transformed.shape[0]-40:x_transformed.shape[0]-17])
-# print(speeds[-50:-17])
-# print("energy:")
-# print(energys[-50:-17])
-# print("y")
-# print(energys_shifted[-30:])
 
 # Split the data into train and test
 X_train, X_test, y_train, y_test = train_test_split(
     x_transformed[:-17], energys_shifted_scaled, test_size=0.2, shuffle=False)
 
 
-# X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
-# X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
-
 # Build model
 model = Sequential()
 
-# model.add(LSTM(12, activation='tanh', input_shape=(
-#     TIMESTEPS, NUM_FEATURES), return_sequences=False, activity_regularizer=l2(0.001)))
-# model.add(Dropout(0.1))
-model.add(LSTM(24, input_shape=(TIMESTEPS, NUM_FEATURES), return_sequences=False, activity_regularizer=l2(0.001)))
-# model.add(Dropout(0.1))
-# model.add(LSTM(32,return_sequences=False))
+model.add(LSTM(24, input_shape=(TIMESTEPS, NUM_FEATURES),
+               return_sequences=False, activity_regularizer=l2(0.001)))
 model.add(Dropout(0.1))
 model.add(Dense(12, activation='relu'))
 model.add(Dense(1, activation='linear'))
-# model.add(LSTM(48,  activation='tanh', input_shape=(TIMESTEPS, 3), return_sequences=False))
-# model.add(Dropout(0.1))
+model.summary()
 
-# 
-# 
+#
+#
 # model.add(LSTM(24,  activation='tanh', input_shape=(
 #     TIMESTEPS, NUM_FEATURES), return_sequences=True, activity_regularizer=l2(0.001)))
 # model.add(LSTM(12,return_sequences=True))
@@ -178,7 +158,7 @@ model.add(Dense(1, activation='linear'))
 # model.add(LSTM(4,return_sequences=False))
 # model.add(Dropout(0.1))
 # model.add(Dense(1, activation='tanh'))
-# model.summary()
+
 
 
 opt = optimizers.Adam(learning_rate=LEARNING_RATE)
@@ -222,11 +202,3 @@ plt.xlabel('Predicted Value')
 plt.xlabel('Actual Value')
 plt.show()
 
-# # Test particular prediction
-# x = 1210
-# print(X_energyDataWithWindow[x])
-# data = scaler_x.transform([X_energyDataWithWindow[x]])
-# data = data.reshape(1, WINDOW_SIZE*2, 1)
-# datay = model.predict(data)
-# print(scaler_y.inverse_transform(datay))
-# print(Y_energyDataWithWindow[x])
